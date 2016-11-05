4ecf7845f72433cc0ab055d886aae9d6ce217768
thelasthero <judgetread@gmail.com>
2016-11-05 15:25:26 +1000

    update

---
+++ src/abraham/MySQLAPI.java
@@ -0,0 +1,17 @@
+package abraham;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class MySQLAPI extends JavaPlugin{
+
+	@Override
+	public void onDisable() {
+		
+	}
+
+	@Override
+	public void onEnable() {
+		
+	}
+
+}
